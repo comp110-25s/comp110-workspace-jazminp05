15f51d3ef0a51381bd4951af3dbc4465e0475c59
Jazmin Puebla <jazminp@ad.unc.edu>
2025-03-25 18:58:34 -0400

    edited test functions

--- exercises/ex03/dictionary_test.py
+++ exercises/ex03/dictionary_test.py
@@ -8,31 +8,46 @@ from exercises.ex03.dictionary import bin_len
 import pytest
 
 
-def test_invert() -> None:
+def test_invert_use_1() -> None:
     assert invert({"a": "z", "b": "y", "c": "x"}) == {"z": "a", "y": "b", "x": "c"}
+
+
+def test_invert_use_2() -> None:
     assert invert({"op": "81", "mv": "1", "cl": "16"}) == {
         "81": "op",
         "1": "mv",
         "16": "cl",
     }
+
+
+def test_invert_empty() -> None:
     assert invert({}) == {}
+
+
+def test_invert_KeyError() -> None:
     with pytest.raises(KeyError):
         my_dict = {"jazmin": "puebla", "jolette": "puebla"}
         assert invert(my_dict) == KeyError
 
 
-def test_count() -> None:
+def test_count_use_1() -> None:
     assert count(["a", "a", "a"]) == {"a": 3}
+
+
+def test_count_use_2() -> None:
     assert count(["apple", "apple", "orange", "cherry", "grape", "cherry"]) == {
         "apple": 2,
         "orange": 1,
         "cherry": 2,
         "grape": 1,
     }
+
+
+def test_count_empty() -> None:
     assert count([]) == {}
 
 
-def test_favortie_color() -> None:
+def test_favortie_color_use() -> None:
     assert (
         favorite_color(
             {
@@ -45,6 +60,9 @@ def test_favortie_color() -> None:
         )
         == "orange"
     )
+
+
+def test_favortie_color_tie() -> None:
     assert (
         favorite_color(
             {
@@ -57,10 +75,19 @@ def test_favortie_color() -> None:
         )
         == "blue"
     )
+
+
+def test_favortie_color_empty() -> None:
     assert favorite_color({}) == ""
 
 
-def test_bin_len() -> None:
+def test_bin_len_use() -> None:
     assert bin_len(["ice", "water", "steam"]) == {3: {"ice"}, 5: {"water", "steam"}}
+
+
+def test_bin_len_use_2() -> None:
     assert bin_len(["sleep", "sleep", "study"]) == {5: {"sleep", "study"}}
+
+
+def test_bin_len_empty() -> None:
     assert bin_len([]) == {}
