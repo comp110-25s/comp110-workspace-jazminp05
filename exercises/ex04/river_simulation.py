8c972316fbf85d45e4cf9991e6b0f0acd36e5176
Jazmin Puebla <jazminp@ad.unc.edu>
2025-04-09 22:51:08 -0400

    Troubleshooting

--- exercises/ex04/river_simulation.py
+++ exercises/ex04/river_simulation.py
@@ -3,7 +3,7 @@ __author__ = "730672588"
 
 """Simulates river class"""
 
-from exercises.ex04.river import River
+from exercises.EX04.river import River
 
 my_river: River = River(10, 2)
 my_river.view_river()
